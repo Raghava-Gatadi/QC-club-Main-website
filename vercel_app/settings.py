cca4713291b441450486782e96d45d08a456c08d
Raghava Gatadi <raghava.gtd@gmail.com>
2023-04-14 14:45:06 +0530

    test

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -126,8 +126,6 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
