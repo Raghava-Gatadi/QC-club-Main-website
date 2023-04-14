02f1b114f3dba57ef5f0dc6295cf058c36ef325b
Raghava Gatadi <raghava.gtd@gmail.com>
2023-04-14 20:26:32 +0530

    db disconnected

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -77,21 +77,11 @@ WSGI_APPLICATION = 'vercel_app.wsgi.app'
 # Note: Django modules for using databases are not support in serverless
 # environments like Vercel. You can use a database over HTTP, hosted elsewhere.
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'qc_website',
-        'USER': 'postgres',
-        'PASSWORD': 'badrikarthik',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 # Password validation
